bc539a4736970600651087322ba2b9b12a898aa3
Gabor Feher <gume@tmit>
2024-09-20 00:59:22 +0200

    New procy server for multiple clients

--- cloud/control/docker-entrypoint.sh
+++ cloud/control/docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -x
 
-#tmux new-session -d -s realhebi "source /opt/ros/humble/setup.bash; cd /opt/ws/;. install/setup.bash;ros2 launch realhebi k8s-realhebi.launch.py; sleep inf"
-
-tmux new-session -d -s sick_frames "source /opt/ws/install/setup.bash && ros2 launch smartamr staticframes.launch.py && sleep inf"
-tmux new-session -d -s sickpublisher "source /opt/ws/install/setup.bash && ros2 run smartamr sickpublisher && sleep inf"
+tmux new-session -d -s video "python3 /root/zmq_videopub.py && sleep inf"
+tmux new-session -d -s data "python3 /root/DOGZILLAProxyServer.py && sleep inf"
 
 tail -f /dev/null
