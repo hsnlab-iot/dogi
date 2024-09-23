753ab3492816fcc0ca65b78271a77b858f78d929
gume <gume75@gmail.com>
2024-09-23 11:35:29 +0000

    replace sit

--- cloud/control/src/sit.py
+++ cloud/control/src/sit.py
@@ -1,4 +1,4 @@
-# Sit motors: [18.25, 9.45, 1.09, 17.24, 10.07, 0.85, -71.98, 93.0, 1.34, -72.49, 93.0, 0.36]
+# Sit motors: [27.43, 1.96, 1.09, 28.45, 1.96, 0.85, -72.49, 93.0, 1.09, -71.98, 93.0, 0.61]
 # 1: Front right, 2: Front left, 3: Back left, 4: Back right
 # x1: Up, x2: Down, x3: Side
 
@@ -16,7 +16,7 @@ dogControl.motor(31, -73)
 dogControl.motor(41, -73)
 dogControl.motor(12, 10)
 dogControl.motor(22, 10)
-dogControl.motor(11, 0)
-dogControl.motor(21, 0)
+dogControl.motor(11, 30)
+dogControl.motor(21, 30)
 time.sleep(1)
 print(dogControl.read_motor())
