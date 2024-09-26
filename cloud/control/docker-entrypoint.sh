91c72ecda75f465aef7370b5e2b83593d992f170
gume <gume75@gmail.com>
2024-09-26 16:20:43 +0000

    dev

--- cloud/control/docker-entrypoint.sh
+++ cloud/control/docker-entrypoint.sh
@@ -20,5 +20,6 @@ tmux new-session -d -s webvoice "cd /root && source .flask/bin/activate && pytho
 tmux new-session -d -s webkeresd "cd /root && source .flask/bin/activate && python3 web_keresd.py ; sleep inf"
 tmux new-session -d -s webkovesd "cd /root && source .flask/bin/activate && python3 web_kovesd.py ; sleep inf"
 tmux new-session -d -s webmutasd "cd /root && source .flask/bin/activate && python3 web_mutasd.py ; sleep inf"
+tmux new-session -d -s webmain "cd /root && source .flask/bin/activate && python3 web_main.py ; sleep inf"
 
 tail -f /dev/null
