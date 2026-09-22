45f3485e9e056cefecf00ed6e6e8823db23c37aa
Gabor Feher <gume75@gmail.com>
2026-09-22 20:46:35 +0000

    update

--- cloud/control/docker-entrypoint.sh
+++ cloud/control/docker-entrypoint.sh
@@ -8,7 +8,7 @@ export PYTHONIOENCODING=UTF-8
 tmux new-session -d -s supervisord "/root/.local/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf"
 tmux new-session -d -s video "python3 /app/zmq_videopub.py ; sleep inf"
 tmux new-session -d -s streamer "cd /app && source /opt/venv/bin/activate && python3 /app/streamer_client.py ; sleep inf"
-tmux new-session -d -s ollama_exporter "cd /app && python3 ollama_exporter.py --ollama-url http://10.6.6.20:11434 --log-level DEBUG ; sleep inf"
+#tmux new-session -d -s ollama_exporter "cd /app && python3 ollama_exporter.py --ollama-url http://10.6.6.20:11434 --log-level DEBUG ; sleep inf"
 
 tmux new-session -d -s data "python3 /app/DOGZILLAProxyServer.py ; sleep inf"
 
@@ -19,7 +19,6 @@ done
 
 tmux new-session -d -s webmain "cd /app && source /opt/venv/bin/activate && python3 web_main.py ; sleep inf"
 tmux new-session -d -s webvideo "cd /app && source /opt/venv/bin/activate && python3 web_video.py ; sleep inf"
-#tmux new-session -d -s webvoice "cd /app && source /opt/venv/bin/activate && python3 web_voice.py ; sleep inf"
 
 tmux new-session -d -s websystem "cd /app && source /opt/venv/bin/activate && python3 web_system.py ; sleep inf"
 tmux new-session -d -s webkeresd "cd /app && source /opt/venv/bin/activate && python3 web_keresd.py ; sleep inf"
